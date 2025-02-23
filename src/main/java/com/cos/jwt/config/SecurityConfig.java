361addf123aa45a818773e7a7f94636208b8bc17
khj20231204 <natista00@naver.com>
2025-02-23 17:04:20 +0900

    jwt

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cos.jwt.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 /* 현재 사용하지 않는 버전 => 최근 버전은 SecurityFilterChain으로 사용
@@ -18,6 +19,11 @@ public class SecurityConfig{
    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       http
          .csrf(csrf -> csrf.disable())
-
+         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+         .and()
+         .formLogin().disable()
+         .httpBasic().disabpe()
+         .authorieRe
+         
    }
 }
